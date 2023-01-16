d0597ecee82017bf0445178968c566c4afff6be2
Caleb <caleb.case93@gmail.com>
2023-01-16 11:21:46 -0800

    Add dropdown selector for spectrograms

--- st_app.py
+++ st_app.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import os
 
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -8,22 +9,30 @@ from src import pipeline
 
 
 # Create spectrograms
-grams = pipeline.ts_to_spectrogram(
-    dt.date(2022, 11, 5),
-    dt.date(2022, 11, 6),
-    'wav'
-)
+# grams = pipeline.ts_to_spectrogram(
+#     dt.date(2022, 11, 5),
+#     dt.date(2022, 11, 6),
+#     'wav'
+# )
+
+grams = [
+    pipeline.create_spectogram('wav/' + file, 1)[2]
+    for file in os.listdir('wav')
+]
 
 # Title
 st.write("# Example Spectrograms")
 
-# Add grams
-for i, gram in enumerate(grams):
-    st.write(f"Spectrogram {i}")
+# Choose spectrogram
+gram_idx = st.selectbox(
+    'Spectrogram #',
+    range(len(grams))
+)
 
-    fig, ax = plt.subplots()
-    plt.pcolormesh(grams[0])
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time')
-
-    st.pyplot(fig)
+# Display
+st.write(f"Spectrogram {gram_idx}")
+fig, ax = plt.subplots()
+plt.pcolormesh(grams[gram_idx])
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time')
+st.pyplot(fig)
