ef3fad2ab3fabb14a1dee0b0f5c2b49959a93e38
mitchhaldeman <mitch@haldeman.us>
2023-01-24 10:47:41 -0800

    changed table index to datetime

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ from skimage.restoration import denoise_wavelet
 import os
 import numpy as np
 import pandas as pd
+import datetime
 
 
 def apply_per_channel_energy_norm(spectrogram):
@@ -126,7 +127,7 @@ def select_spec_case(plot_path, folder_path, pcen=False, wavelet=False):
             spectrogram_data = wavelet_denoising(pcen_spec)
         spec_plot_and_save(spectrogram_data, f_name, plot_path)
 
-def wav_to_array(filepath, hop_length = 256, n_fft=4800, pcen=False, wavelet=False, ref=np.max):
+def wav_to_array(filepath, t0=datetime.datetime.now(), hop_length = 256, n_fft=4800, pcen=False, wavelet=False, ref=np.max):
     """
     This function converts a wavfile to a dataframe of power spectral density, with the index as the timestamp from the start of the wav file and the columns as the frequency bin.  This function also calculates the broadband average noise level of the input wavefile before the dB conversion per time step after the FFT calculation.  
 
@@ -135,6 +136,7 @@ def wav_to_array(filepath, hop_length = 256, n_fft=4800, pcen=False, wavelet=Fal
 
     Args:
         filepath: file path to .wav
+        to: datetime.  starting time of the recording. 
         hop_length: int. number of audio samples between adjacent STFT columns.  See librosa docs for more info. 
         n_fft: int. number of points in the acquired time-domain signal.  delta F = sample rate/n_fft
         pcen: binary. set to True to apply PCEN
@@ -149,7 +151,8 @@ def wav_to_array(filepath, hop_length = 256, n_fft=4800, pcen=False, wavelet=Fal
     D_highres = librosa.stft(y, hop_length=hop_length, n_fft=n_fft)
     spec = librosa.amplitude_to_db(np.abs(D_highres), ref=ref)
     freqs = librosa.core.fft_frequencies(sr=sr,n_fft=n_fft)
-    times = librosa.core.frames_to_time(np.arange(spec.shape[1]), sr=sr, n_fft=n_fft, hop_length=hop_length)
+    secs = librosa.core.frames_to_time(np.arange(spec.shape[1]), sr=sr, n_fft=n_fft, hop_length=hop_length)
+    times = [t0 + datetime.timedelta(seconds=x) for x in secs]
 
     if pcen:
         spec = librosa.core.pcen(spec)
@@ -166,7 +169,6 @@ def wav_to_array(filepath, hop_length = 256, n_fft=4800, pcen=False, wavelet=Fal
     df = pd.DataFrame(spec.transpose(), columns=freqs, index=times)
     df = df.astype(float).round(2)
     df.columns = df.columns.map(str)
-    print('hellooooo')
 
     avg_df = pd.DataFrame(avgs, index=times)
     avg_df = avg_df.astype(float).round(2)
