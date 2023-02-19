2d4cbfac00a80364e5ae20b2dd1b84cd2c9c54f2
Caleb <caleb.case93@gmail.com>
2023-02-19 08:49:20 -0800

    Add name to hydrophone info

--- src/hydrophone.py
+++ src/hydrophone.py
@@ -6,10 +6,10 @@ class Hydrophone(Enum):
     Enum for orcasound hydrophones, including AWS S3 Bucket info
     """
 
-    HPhoneTup = namedtuple("Hydrophone", "bucket ref_folder")
+    HPhoneTup = namedtuple("Hydrophone", "name bucket ref_folder")
 
-    BUSH_POINT = HPhoneTup("streaming-orcasound-net", "rpi_bush_point")
-    ORCASOUND_LAB = HPhoneTup("streaming-orcasound-net", "rpi_orcasound_lab")
-    PORT_TOWNSEND = HPhoneTup("streaming-orcasound-net", "rpi_port_townsend")
-    SUNSET_BAY = HPhoneTup("streaming-orcasound-net", "rpi_sunset_bay")
-    SANDBOX = HPhoneTup("acoustic-sandbox", "orcasounds")
+    BUSH_POINT = HPhoneTup("bush_point", "streaming-orcasound-net", "rpi_bush_point")
+    ORCASOUND_LAB = HPhoneTup("orcasound_lab", "streaming-orcasound-net", "rpi_orcasound_lab")
+    PORT_TOWNSEND = HPhoneTup("port_townsend", "streaming-orcasound-net", "rpi_port_townsend")
+    SUNSET_BAY = HPhoneTup("sunset_bay", "streaming-orcasound-net", "rpi_sunset_bay")
+    SANDBOX = HPhoneTup("sandbox", "acoustic-sandbox", "orcasounds")
