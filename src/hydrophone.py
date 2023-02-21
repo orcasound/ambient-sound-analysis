961a4e7b9cd92e2153e344277161b87fef7acba0
Caleb <caleb.case93@gmail.com>
2023-02-21 14:02:03 -0800

    Add write destination settings to Hydrophone Enum

--- src/hydrophone.py
+++ src/hydrophone.py
@@ -6,10 +6,10 @@ class Hydrophone(Enum):
     Enum for orcasound hydrophones, including AWS S3 Bucket info
     """
 
-    HPhoneTup = namedtuple("Hydrophone", "name bucket ref_folder")
+    HPhoneTup = namedtuple("Hydrophone", "name bucket ref_folder save_bucket save_folder")
 
-    BUSH_POINT = HPhoneTup("bush_point", "streaming-orcasound-net", "rpi_bush_point")
-    ORCASOUND_LAB = HPhoneTup("orcasound_lab", "streaming-orcasound-net", "rpi_orcasound_lab")
-    PORT_TOWNSEND = HPhoneTup("port_townsend", "streaming-orcasound-net", "rpi_port_townsend")
-    SUNSET_BAY = HPhoneTup("sunset_bay", "streaming-orcasound-net", "rpi_sunset_bay")
-    SANDBOX = HPhoneTup("sandbox", "acoustic-sandbox", "orcasounds")
+    BUSH_POINT = HPhoneTup("bush_point", "streaming-orcasound-net", "rpi_bush_point", "acoustic-sandbox", "bush_point")
+    ORCASOUND_LAB = HPhoneTup("orcasound_lab", "streaming-orcasound-net", "rpi_orcasound_lab", "acoustic-sandbox", "orcasound_lab")
+    PORT_TOWNSEND = HPhoneTup("port_townsend", "streaming-orcasound-net", "rpi_port_townsend", "acoustic-sandbox", "port_townsend")
+    SUNSET_BAY = HPhoneTup("sunset_bay", "streaming-orcasound-net", "rpi_sunset_bay", "acoustic-sandbox", "sunset_bay")
+    SANDBOX = HPhoneTup("sandbox", "acoustic-sandbox", "orcasounds", "acoustic-sandbox", "orcasounds")
