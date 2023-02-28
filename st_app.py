0b17c8e03cb077fdc49af2746bee5eb6711a78e6
Caleb <caleb.case93@gmail.com>
2023-02-28 10:49:26 -0800

    Band range selector and round times

--- st_app.py
+++ st_app.py
@@ -21,18 +21,19 @@ selected_hydrophone = st.selectbox(
 # Get Dfs
 @st.cache
 def get_summary_dfs(hydrophone):
-    return daily_noise.create_daily_noise_summary_df(dt.date(2023, 2, 1), 2)
+    return daily_noise.create_daily_noise_summary_df(dt.date(2023, 2, 1), 3)
 summary_dfs = get_summary_dfs(selected_hydrophone)
 
 # Choose Band
 bands = summary_dfs["mean"].columns
 selected_band = st.select_slider(
     'Band',
-    bands
+    bands, 
+    (bands[0], bands[-1])
 )
 
 # Display
-st.write(f"Daily Noise in {selected_band}hz band")
+st.write(f"Daily Noise in {selected_band[0]}hz to {selected_band[1]}hz band")
 fig = daily_noise.plot_daily_noise(summary_dfs, band=selected_band)
 fig.patch.set_facecolor(None)
 fig.patch.set_alpha(0.0)
