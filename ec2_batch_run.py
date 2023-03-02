dae9f76569b622cfa71ededff55065e5bcc2992d
Grant Savage <66881605+savageGrant@users.noreply.github.com>
2023-03-01 16:55:18 -0800

    pulls in latest from main

--- ec2_batch_run.py
+++ ec2_batch_run.py
@@ -13,7 +13,7 @@ import time
 import os
 
 orcasound_lab = hydrophone.Hydrophone.ORCASOUND_LAB
-ship = pipeline.NoiseAnalysisPipeline(orcasound_lab, delta_f=10, delta_t=1, bands=3, )
+ship = pipeline.NoiseAnalysisPipeline(orcasound_lab, delta_f=10, delta_t=1, bands=3,)
 
 start_time = datetime.datetime(2023,2,10,22,00,00)
 end_time = datetime.datetime(2023,2,10,23,00,00)
