4d31f831c20fb16ec6176c35e877aca2fe0f8325
Grant Savage <66881605+savageGrant@users.noreply.github.com>
2023-03-01 16:49:18 -0800

    adds batch run for ec2

---
+++ ec2_batch_run.py
@@ -0,0 +1,21 @@
+
+from src import acoustic_util
+from src import pipeline
+from src import hydrophone
+import librosa
+from src import acoustic_util
+import pandas as pd
+import numpy as np
+import datetime
+import scipy.signal
+from orca_hls_utils.DateRangeHLSStream import DateRangeHLSStream
+import time
+import os
+
+orcasound_lab = hydrophone.Hydrophone.ORCASOUND_LAB
+ship = pipeline.NoiseAnalysisPipeline(orcasound_lab, delta_f=10, delta_t=1, bands=3, )
+
+start_time = datetime.datetime(2023,2,10,22,00,00)
+end_time = datetime.datetime(2023,2,10,23,00,00)
+
+ship_noise_psd, ship_noise_broad = ship.generate_psds(start=start_time, end=end_time)
