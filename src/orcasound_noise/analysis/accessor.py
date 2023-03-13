ab7157b0d8d5145f56d724ea5e421178377cf017
mitchhaldeman <mitch@haldeman.us>
2023-03-13 16:04:14 -0700

    added ambient scaling, dropdown constraints

--- src/orcasound_noise/analysis/accessor.py
+++ src/orcasound_noise/analysis/accessor.py
@@ -80,3 +80,29 @@ class NoiseAccessor:
         diff = target_dt.second - seconds
         
         return target_dt.replace(microsecond=0) - dt.timedelta(seconds=diff)
+    
+    def get_options(self):
+        """
+        Get the time delta and frequency resolution options available already in S3. 
+
+        # Return
+        Tuple of lists.  Delta_t values, Delta_f values, frequency types. 
+        """
+        delta_fs = []
+        delta_ts = []
+        freq_types = []
+
+        for item in self.connector.archive_resource.objects.filter(Prefix=self.connector.save_folder):
+            filename = item.key.split("/")[-1]
+            args = filename.replace(".parquet", "").split("_")
+
+            if args[0] == 'ancient':
+                continue
+            else:
+                _, _, secs, freq_value, freq_type = self.connector.parse_filename(filename)
+                if freq_value != 0:
+                    delta_fs.append(freq_value)
+                delta_ts.append(secs)
+                freq_types.append(freq_type)
+
+        return list(set(delta_ts)), list(set(delta_fs)), list(set(freq_types))
