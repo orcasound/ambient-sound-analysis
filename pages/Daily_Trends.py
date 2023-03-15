575f99bf562ffede3fa1a3a2c592d41d708ee959
Caleb <caleb.case93@gmail.com>
2023-03-15 14:42:32 -0700

    Pre-compute daily trends

--- pages/Daily_Trends.py
+++ pages/Daily_Trends.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from copy import deepcopy
+import pickle
 
 import streamlit as st
 import pandas as pd
@@ -13,8 +14,9 @@ from src.orcasound_noise.pipeline import acoustic_util
 # Title
 st.write("# Daily Noise Levels")
 
-hydro_col, ref_col = st.columns([2,1])
-with hydro_col:
+
+col1, col2 = st.columns(2)
+with col1:
     # Choose Hydrophone
     hydrophones = ["Orcasound Lab"]
     selected_hydrophone = st.selectbox(
@@ -22,45 +24,22 @@ with hydro_col:
         hydrophones
     )
 
-with ref_col:
-    reference = st.radio(
-        'Reference Level',
-        ('Full Scale', 'Ancient Ambient'),
-        index=1,
-        key='daily_ref'
-    )
-
-if reference == 'Ancient Ambient':
-    aa = True
-else:
-    aa = False
-
-# Choose dates
-col1, col2, col3 = st.columns(3)
-anchor_date = dt.date(2021, 1, 1)
-with col1:
-    start_date = st.date_input(
-        "Start Date", 
-        anchor_date, 
-        min_value=anchor_date, 
-        max_value=dt.date.today()
-    )
 with col2:
-    end_date = st.date_input(
-        "End Date", 
-        start_date + dt.timedelta(days=7), 
-        min_value=start_date, 
-        max_value=dt.date.today()
+    # Choose Month
+    months = [f"2021_{i}" for i in range(1,12)]
+    selected_month = st.selectbox(
+        'Month',
+        months
     )
-day_range = (end_date - start_date).days
 
 # Create analysis
 @st.cache
-def get_summary_dfs(hydrophone):
+def get_summary_dfs(hydrophone, month):
+    with open(f'pages/demo_data/daily_summary_{month}.pkl', 'rb') as f:
+        return pickle.load(f)
     return DailyNoiseAnalysis(Hydrophone[hydrophone.upper().replace(" ", "_")]).create_daily_noise_summary_df(start_date, day_range)
-summary_dfs = deepcopy(get_summary_dfs(selected_hydrophone))
-with col3:
-    st.write(f"{summary_dfs['count'].max().max()} days of data found within range")
+summary_dfs = get_summary_dfs(selected_hydrophone, selected_month)
+
 
 # Choose Band
 bands = summary_dfs["mean"].columns
@@ -70,35 +49,20 @@ selected_band = st.select_slider(
     (bands[0], bands[-1])
 )
 
-if aa:
-    aa_df = summary_dfs
-    ship = pipeline.NoiseAnalysisPipeline(Hydrophone[selected_hydrophone.upper().replace(" ", "_")], delta_f=1,delta_t = 1, no_auth=True)
-
-    aa = ship.get_ancient_ambient(dt.datetime.combine(end_date,dt.time(0,0,0)))
-    aa_df['mean'] = acoustic_util.dBFS_to_aa(aa_df['mean'], aa)
-    aa_df['min'] = acoustic_util.dBFS_to_aa(aa_df['min'], aa)
-    aa_df['max'] = acoustic_util.dBFS_to_aa(aa_df['max'], aa)
-
-    out_df = aa_df
-else:
-    out_df = summary_dfs
 
 # Display
 st.write(f"Daily Noise in {selected_band[0]}hz to {selected_band[1]}hz band")
-fig = DailyNoiseAnalysis.plot_daily_noise(out_df, band=selected_band, mean_smoothing=100, error_smoothing=10)
+fig = DailyNoiseAnalysis.plot_daily_noise(summary_dfs, band=selected_band, mean_smoothing=100, error_smoothing=10)
 st.plotly_chart(fig)
 
 # Broadband
 st.write("Average Daily Noise Levels")
+year, month = selected_month.split("_")
+start_date = dt.date(int(year), int(month), 1)
 @st.cache
 def get_broadband_df(hydrophone):
-    return DailyNoiseAnalysis(Hydrophone[hydrophone.upper().replace(" ", "_")]).create_broadband_daily_noise(start_date, day_range)
-broadband_df = deepcopy(get_broadband_df(selected_hydrophone))
-if aa:
-    aa_bb = ship.get_ancient_ambient(dt.datetime.combine(end_date,dt.time(0,0,0)), dB=False)
-    bb_df = acoustic_util.abs_to_dB(broadband_df, ref=aa_bb, columns=['Level'])
-else:
-    bb_df = acoustic_util.abs_to_dB(broadband_df, columns=['Level'])
+    return DailyNoiseAnalysis(Hydrophone[hydrophone.upper().replace(" ", "_")]).create_broadband_daily_noise(start_date, 30)
+broadband_df = get_broadband_df(selected_hydrophone)
 
-fig = DailyNoiseAnalysis.plot_broadband_daily_noise(bb_df)
+fig = DailyNoiseAnalysis.plot_broadband_daily_noise(broadband_df)
 st.plotly_chart(fig)
