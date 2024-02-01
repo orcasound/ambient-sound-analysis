81146a85bb52b9e53d03c1377b70aa5473c4e8ec
zprice12 <118499445+zprice12@users.noreply.github.com>
2024-02-01 14:39:28 -0800

    Update test_viz.py

--- testing_scripts/zach/test_viz.py
+++ testing_scripts/zach/test_viz.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 data_msds = pd.read_csv('/Users/zach/Desktop/row.txt', delimiter=' ', header=None)
 data_ben = pd.read_csv('/Users/zach/Desktop/rpi-port-townsend_2023_03_22_11_05_25_psd.txt', delimiter=' ', header=None)
 data_val = pd.read_csv('/Users/zach/Downloads/psdCSVfile.csv', header=None)
-print(data_val)
 
 plt.plot(data_ben.loc[0])
 plt.plot(data_msds)
