99f687f9bc75e848f1d68d9c87b64f257fe0687a
Vaibhav Mehrotra <vaibhav1@uw.edu>
2024-03-04 18:11:12 -0800

    feat(test_3.py): Add time benchmarking

--- test_3.py
+++ test_3.py
@@ -1,22 +1,28 @@
 import pandas as pd
-from src.orcasound_noise.pipeline.pipeline_3 import NoiseAnalysisPipeline
-from src.orcasound_noise.utils import Hydrophone
-from src.orcasound_noise.pipeline.acoustic_util import plot_spec
+from orcasound_noise.pipeline.pipeline import NoiseAnalysisPipeline
+from orcasound_noise.utils import Hydrophone
+from orcasound_noise.pipeline.acoustic_util import plot_spec
 import datetime as dt
 import matplotlib.pyplot as plt
+import time
 
 if __name__ == '__main__':
-    pipeline = NoiseAnalysisPipeline(Hydrophone.PORT_TOWNSEND, pqt_folder='/Users/zach/Downloads/',
-                                     wav_folder='/Users/zach/Downloads/', delta_f=1, bands=None,
-                                     delta_t=60)
+    ts = time.time()
+    pipeline = NoiseAnalysisPipeline(Hydrophone.PORT_TOWNSEND, pqt_folder='/Users/vmehrotra/Desktop/ambient-sound-analysis/test_folder',
+                                     wav_folder='/Users/vmehrotra/Desktop/ambient-sound-analysis/test_folder', 
+                                     delta_f=1, bands=None,
+                                     delta_t=60, mode='safe')
 
-    psd_path, broadband_path = pipeline.generate_parquet_file(dt.datetime(2023, 3, 22, 11, 0),
-                                                              dt.datetime(2023, 3, 22, 12, 0), upload_to_s3=False)
+    psd_path, broadband_path = pipeline.generate_parquet_file(dt.datetime(2023, 3, 22, 11),
+                                                              dt.datetime(2023, 3, 22, 12), upload_to_s3=False)
 
     bb_df = pd.read_parquet(broadband_path)
     psd_df = pd.read_parquet(psd_path)
+    print("#"*10, 'Shape: ', psd_df.shape)
+    print("Execution Time", time.time()-ts)
+    # plot_spec(psd_df)
 
-    plot_spec(psd_df)
+    # plt.plot(bb_df)
+    # plt.show()
 
-    plt.plot(bb_df)
-    plt.show()
+    ########## Shape:  (51, 24001)
