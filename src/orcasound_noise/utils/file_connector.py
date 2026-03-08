1b583621bbec96b43626c7cdc9493d22eb7d5124
erinmee <erinmee@uw.edu>
2026-03-08 14:23:16 -0700

    small change to ref file name pointer

--- src/orcasound_noise/utils/file_connector.py
+++ src/orcasound_noise/utils/file_connector.py
@@ -218,8 +218,8 @@ class S3FileConnector:
     
     def get_ref_file(self, hydrophone: Hydrophone, location: str):
         self.client.download_file(self.save_bucket,
-                                    f'{hydrophone.value.save_folder}/ref/hydrophone={hydrophone.value.name}/{hydrophone.value.name}_refs.parquet',
-                                    f'{location}/{hydrophone.value.name}_refs.parquet')
+                                    f'{hydrophone.value.save_folder}/ref/hydrophone={hydrophone.value.name}/{hydrophone.value.name}_ref.parquet',
+                                    f'{location}/{hydrophone.value.name}_ref.parquet')
     
 
 class ShipMetricsS3Connector:
