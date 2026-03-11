e653d865a162eb8236fb12f0178ad77a7c03014e
bradyhhhuang <bradyhuang053@gmail.com>
2026-03-10 20:47:11 -0700

    modify based on suggestions

--- src/orcasound_noise/analysis/partitioned_accessor.py
+++ src/orcasound_noise/analysis/partitioned_accessor.py
@@ -4,16 +4,14 @@ import datetime as dt
 from datetime import timedelta
 
 from ..utils import Hydrophone
+from orcasound_noise.pipeline.acoustic_util import octave_band
 
 class PartitionedAccessor:
-    def __init__(self, hydrophone: Hydrophone, start_time: dt.datetime, end_time: dt.datetime, s3_folder: str = None):
+    def __init__(self, hydrophone: Hydrophone, start_time: dt.datetime, end_time: dt.datetime):
         self.hydrophone = hydrophone
         self.start_time = start_time
         self.end_time = end_time
-        if s3_folder:
-            s3_loc = f"s3://{hydrophone.value.save_bucket}/{s3_folder}"
-        else:
-            s3_loc = f"s3://{hydrophone.value.save_bucket}/{hydrophone.value.save_folder}"
+        s3_loc = f"s3://{hydrophone.value.save_bucket}/{hydrophone.value.save_folder}"
         psd_paths = []
         bb_paths = []
         d = start_time.date()
@@ -57,17 +55,7 @@ class PartitionedAccessor:
             dict: A dictionary representing the schema for the PSD data.
         '''
         if freqs is None:
-            freqs = [
-                67, 71, 75, 80, 85, 90, 95, 100, 106, 112, 118, 125,
-                132, 140, 150, 160, 170, 180, 190, 200, 212, 224, 236, 250,
-                265, 280, 300, 315, 335, 355, 375, 400, 425, 450, 475, 500,
-                530, 560, 600, 630, 670, 710, 750, 800, 850, 900, 950, 1000,
-                1060, 1120, 1180, 1250, 1320, 1400, 1500, 1600, 1700, 1800, 1900, 2000,
-                2120, 2240, 2360, 2500, 2650, 2800, 3000, 3150, 3350, 3550, 3750, 4000,
-                4250, 4500, 4750, 5000, 5300, 5600, 6000, 6300, 6700, 7100, 7500, 8000,
-                8500, 9000, 9500, 10000, 10600, 11200, 11800, 12500, 13200, 14000, 15000, 16000,
-                17000, 18000, 19000, 20000, 21200, 22400
-            ]
+            freqs, _ = octave_band(12, [])
             
         schema = {"ind": pl.Datetime('ns')}
         for freq in freqs:
